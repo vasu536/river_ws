a35a9cf6160601aee2e534024f78aaec809a0db7
vasu536 <vasu.karuparthi@gmail.com>
2018-02-27 10:28:15 -0500

    More bag files, launch file for both nodes added

--- src/bagfiles/orientus/test.m
+++ src/bagfiles/orientus/test.m
@@ -2,10 +2,12 @@ clc;
 close all;
 clear;
  
-str1 = '/home/vasu536/river_ws/src/bagfiles/orientus/02-23-2018/tb_s_';
+str1 = '/home/vasu536/river_ws/src/bagfiles/orientus/02-26-2018/position_3/tb_s_';
 str3 = '.bag';
 
-n = 8; %number of files in folder in order
+g = 9.80665;
+
+n = 6; %number of files in folder in order
 
 for i = 1:n
     
@@ -13,13 +15,13 @@ for i = 1:n
     
     [x_lin_acc, y_lin_acc] = data_analysis(file);
     
-    figure;
-    plot(x_lin_acc);
-    title('linear acceleration in X direction');
-
-    figure;
-    plot(y_lin_acc);
-    title('linear acceleration in Y direction');
+%     figure;
+%     plot(x_lin_acc);
+%     title('linear acceleration in X direction');
+% 
+%     figure;
+%     plot(y_lin_acc);
+%     title('linear acceleration in Y direction');
 
     x_mean = mean(x_lin_acc);
     y_mean = mean(y_lin_acc);
@@ -30,13 +32,13 @@ for i = 1:n
     x_lin_acc_wobias = x_lin_acc - mean(x_mean);
     y_lin_acc_wobias = y_lin_acc - mean(y_mean);
     
-    figure;   
-    plot(x_lin_acc_wobias);
-    title('linear acceleration in X direction without bias');
-
-    figure;
-    plot(y_lin_acc_wobias);
-    title('linear acceleration in Y direction without bias');
+%     figure;   
+%     plot(x_lin_acc_wobias);
+%     title('linear acceleration in X direction without bias');
+% 
+%     figure;
+%     plot(y_lin_acc_wobias);
+%     title('linear acceleration in Y direction without bias');
 
     std_x_wobias = std(x_lin_acc_wobias);
     std_y_wobias = std(y_lin_acc_wobias);
@@ -54,5 +56,8 @@ figure;
 plot(y_mean_array, '*');
 title('variation of mean of linear acceleration in y');
 
+range_x_mean = max(x_mean_array) - min(x_mean_array);
+range_y_mean = max(y_mean_array) - min(y_mean_array);
 
+theta = rad2deg(-mean(x_mean_array)/g);
 
